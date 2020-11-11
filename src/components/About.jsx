8887e671c56e3f1763de2ad8ffe286eae3991b3c
angiedanielar <angie.ruiz@mail.escuelaing.edu.co>
2020-11-10 21:07:28 -0500

    acerca de nosotros

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,29 +6,27 @@ export default function About() {
             <div className="text-center heading-section mt-5 mb-4">
                 <div className="pl-lg-3 ml-md-5">
                     <span className="subheading">Acerca de</span>
-                    <h2 className="mb-4 ">Sale It</h2>
+                    <h2 className="mb-4 ">Saleit</h2>
                 </div>
             </div>
-
-            <div align="center" className="col-sm-25 wrap-about">
-
-                <div className="col-md-12">
-                    <h5>SaleIt es un proyecto para la
-                        asignatura <strong> Arquitecturas de Software</strong> de la <strong> Escuela Colombiana
-                            de Ingeniería Julio Garavito.</strong></h5>
-                    <br/>
-
-                    <h5>
-                        ¡Subastas online para ofertar y pujar artículos en tiempo real!
-                    </h5>
-
+            <div className="pro-image">
+                <div className="col-sm-8 wrap-about py-5">
+                    <div className="row pb-5 pb-md-0">
+                        <div className="col-md-12 col-lg-6">
+                            <p className="text-justify"> SaleIt es una aplicación web realizada como proyecto para la
+                            asignatura Arquitecturas de Software de la Escuela Colombiana de Ingeniería Julio
+                            Garavito. Somos una aplicación para realizar subastas de forma online donde podrás
+                            ofertar artículos y podrás pujar para conseguir otros. Ofrecemos una fácil usabilidad,
+                            excelente experiencia de usuario y además la participación dentro de las subastas al
+                            momento de ofrecer una puja es de forma anónima. Estos factores nos diferencian respecto
+                                a la mayoría de las páginas de subastas online.</p>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-
             <div className="imagen" align="center">
                 <img src='/img/auction2.png' alt="auction2"></img>
             </div>
-        </div>
+        </div> 
     );
 }
