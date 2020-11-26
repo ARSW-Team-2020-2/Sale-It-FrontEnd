c13bf3328203f423bf5baf9c1d137e356a67f57a
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2020-11-25 20:51:37 -0500

    Intentando integración CrazyEgg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Routes from './components/Routes'
 
 function App() {
   return (
+  	<script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0100/9605.js" async="async" ></script>
     <div className="App">
       <Routes></Routes>
     </div>
