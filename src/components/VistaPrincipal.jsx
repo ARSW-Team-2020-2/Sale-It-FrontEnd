e8d0bd765f4e1ded808591fa50714b12e7c09c38
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2020-11-25 20:37:16 -0500

    Probando integración CrazyEgg

--- src/components/VistaPrincipal.jsx
+++ src/components/VistaPrincipal.jsx
@@ -8,6 +8,9 @@ import ContactPanel from './ContactPanel';
 export default function VistaPrincipal() {
 
     return (
+    <head>
+    <script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0100/9605.js" async="async" ></script>
+    <head>
         <div>
             <Aplicacion/>
             <About/>
