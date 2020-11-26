a29c6f512249735a2af2dac6d884755f96a88e62
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2020-11-25 20:40:56 -0500

    Deshaciendo cambios

--- src/components/VistaPrincipal.jsx
+++ src/components/VistaPrincipal.jsx
@@ -7,10 +7,7 @@ import ContactPanel from './ContactPanel';
 
 export default function VistaPrincipal() {
 
-    return (
-    <head>
-    <script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0100/9605.js" async="async" ></script>
-    <head>
+    return (    
         <div>
             <Aplicacion/>
             <About/>
