fec6a5d4b89e5383c64ce07c84cdf55c53d255ec
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2020-11-25 21:04:51 -0500

    Prueba 3 CrazyEgg

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Routes from './components/Routes'
 
 function App() {
-  return (  	
+  return (
+  <script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0100/9605.js" async="async" ></script>  	
     <div className="App">
       <Routes></Routes>
     </div>
